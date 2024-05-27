7a9c8c9a28b8a2271d0dd43ffa753481464177c4
Brian Jia <brian.jia04@gmail.com>
2024-05-26 22:17:48 -0400

    Remove bluepad32

--- components/bluepad32/arch/uni_log_posix.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2022 Ricardo Quesada
-// http://retro.moe/unijoysticle2
-
-#include "uni_log.h"
-
-void uni_logv(const char* format, va_list args) {
-    vfprintf(stdout, format, args);
-}
